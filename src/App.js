473a6692efa5d7499121a9b6a4aa810959accf0c
ianiee11 <165010372+ianiee11@users.noreply.github.com>
2024-06-13 04:19:52 -1000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './components/Home';
-import Login from './components/Login';
-import Register from './components/Register';
-import Order from './components/Order';
+import About from './components/About';
+import Contact from './components/Contact';
+import Login from './components/LoginForm';
+import Register from './components/RegisterForm';
+import Order from './components/OrderList';
 
 const App = () => {
   return (
@@ -13,6 +15,8 @@ const App = () => {
           <h1>My Writing Service</h1>
           <nav>
             <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+            <Link to="/contact">Contact</Link>
             <Link to="/login">Login</Link>
             <Link to="/register">Register</Link>
             <Link to="/order">Order</Link>
@@ -21,6 +25,8 @@ const App = () => {
         <main>
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
             <Route path="/order" element={<Order />} />
