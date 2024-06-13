329345efc3b2186b1cfef0ae03e011f78533b083
ianiee11 <165010372+ianiee11@users.noreply.github.com>
2024-06-13 04:06:56 -1000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './components/Home';
+import Login from './components/Login';
+import Register from './components/Register';
+import Order from './components/Order';
 
-const App = ({ children }) => {
+const App = () => {
   return (
-    <div>
-      <header>
-        <h1>My Writing Service</h1>
-        <nav>
-          <a href="/">Home</a>
-          <a href="/login">Login</a>
-          <a href="/register">Register</a>
-          <a href="/order">Order</a>
-        </nav>
-      </header>
-      <main>{children}</main>
-    </div>
+    <Router>
+      <div>
+        <header>
+          <h1>My Writing Service</h1>
+          <nav>
+            <Link to="/">Home</Link>
+            <Link to="/login">Login</Link>
+            <Link to="/register">Register</Link>
+            <Link to="/order">Order</Link>
+          </nav>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/order" element={<Order />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
   );
 };
 
