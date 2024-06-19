4aeb420cc42cab83b719b4889488e0c98657f5de
ianiee11 <165010372+ianiee11@users.noreply.github.com>
2024-06-19 05:03:49 -1000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Updated import
-
+import { Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Login from './components/LoginForm';
 import Register from './components/RegisterForm';
@@ -8,28 +7,22 @@ import Order from './components/OrderForm';
 import Contact from './components/Contact';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import LoginForm from './components/LoginForm';
-import RegisterForm from './components/RegisterForm';
-import OrderForm from './components/OrderForm';
-import OrderList from './components/OrderList';
 
 const App = () => {
   return (
-    <Router>
-      <div>
-        <Header />
-        <main>
-          <Routes> {/* Updated to Routes */}
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/order" element={<Order />} />
-            <Route path="/contact" element={<Contact />} />
-          </Routes> {/* Updated to Routes */}
-        </main>
-        <Footer />
-      </div>
-    </Router>
+    <div>
+      <Header />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/order" element={<Order />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </main>
+      <Footer />
+    </div>
   );
 };
 
