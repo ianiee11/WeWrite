9013a73032da047bfe20c32ee1bada2d0dfe1b45
ianiee11 <165010372+ianiee11@users.noreply.github.com>
2024-06-19 05:03:24 -1000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './App';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
@@ -9,14 +9,14 @@ import OrderPage from './pages/OrderPage';
 
 ReactDOM.render(
   <Router>
-    <App>
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/login" component={LoginPage} />
-        <Route path="/register" component={RegisterPage} />
-        <Route path="/order" component={OrderPage} />
-      </Switch>
-    </App>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route index element={<HomePage />} />
+        <Route path="login" element={<LoginPage />} />
+        <Route path="register" element={<RegisterPage />} />
+        <Route path="order" element={<OrderPage />} />
+      </Route>
+    </Routes>
   </Router>,
   document.getElementById('root')
 );
